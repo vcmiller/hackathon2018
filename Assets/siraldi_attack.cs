d6dc2290bfcb4265a209a3ceddf2376c7e3b6463
Nathan Drewniak <nathan.drewniak@gmail.com>
2018-01-13 13:38:34 -0500

    beam

--- Assets/siraldi_attack.cs
+++ Assets/siraldi_attack.cs
@@ -6,18 +6,19 @@ using SBR;
 public class siraldi_attack : BasicMotor<CharacterChannels> {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+
 
 	public override void TakeInput(){
-		print ("here");
 
 		LineRenderer beam = GetComponentInChildren<LineRenderer> ();
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
 
 		if (channels.attack) { 
-			beam.SetPosition (1, new Vector3 (9, 0, 0));
-			print("in honor of vincent: asdf");
+			if(sprite.flipX)
+				beam.SetPosition (1, new Vector3 (-9, 0, 0));
+			else
+				beam.SetPosition (1, new Vector3 (9, 0, 0));
+			
 		}
 		else
 			beam.SetPosition (1, new Vector3 (1, 0, 0));
